18b8233a6010a20bf8fa8ea30e112698645070be
Kate Zasulevich <katejidkova@gmail.com>
2018-02-27 22:44:40 +0300

    found last opened

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
+//module.exports = 
+	function check(str, bracketsConfig) {
   // your solution
+	let arr = str.split("");
+	if(arr.length == 1) return false;
+	let arrLength = arr.length;
+	let lastOpen, lastIndexOpen;
+	
+	for( let i = 0; i < arrLength; i++){
+		if(arr[i] == "(" || arr[i] == "{" || arr[i] == "["){
+			lastOpen = arr[i];
+			lastIndexOpen = i;
+			console.log(lastOpen + " " + lastIndexOpen);
+			
+		}
+	}
 }
+
+check('((()))()', [['(', ')']]) // -> true
