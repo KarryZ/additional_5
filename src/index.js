25b744a2d9ab35b9f40743acc8198f7ab342c754
Kate Zasulevich <katejidkova@gmail.com>
2018-02-28 23:18:21 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
 //module.exports = 
-    function check(str, bracketsConfig) {
-        // your solution
-        let arr = str.split("");
-        if (arr.length % 2 !== 0) return false;
-        let lastOpen, nextClosed;
+function check(str, bracketsConfig) {
+	// your solution
+	let arr = str.split("");
+	if (arr.length % 2 !== 0) return false;
+	
+	let lastOpen, nextClosed;
+	let braketsObj = {};
+	
+	for (let j = 0; j < bracketsConfig.length; j++) {
+		braketsObj[bracketsConfig[j][0]] = bracketsConfig[j][1];
+	};
+	
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == braketsObj["("]){
+			lastOpen = arr[i];		
+			nextClosed = braketsObj["("];
+		} 
+		if (arr[i] == braketsObj["{"]){
+			lastOpen = arr[i];
+			nextClosed = braketsObj["{"];
+		} 
+		if (arr[i] == braketsObj["["]){
+			lastOpen = arr[i];
+			nextClosed = braketsObj["["];
+		} 
+		
+		if (lastOpen !== nextClosed && nextClosed == arr[i + 1]){
+			arr.splice(i, 2);
+			
+		} 			
+	
+	}
 
-        
-            
-             book:   for (let i = 0; i < arr.length; i++) {
-                    for (let j = 0; j < bracketsConfig.length; j++) {
-                        let configEl = bracketsConfig[j][0];
-                        if (arr[i] == bracketsConfig[j][0]) {
-                            lastOpen = arr[i];
-                            nextClosed = arr[i + 1];
-                            if (lastOpen !== nextClosed) {
-                                
-                                if (nextClosed == bracketsConfig[j][1]) {
-                                    arr.splice(i, 2);
-                                    continue book;
-                                }
-                            }
-                        }
-                    }
-                    if (arr.length !== 0) continue book;
-                }
-               
-            
-       
-        if (arr.length == 0){
-            console.log(true); 
-            return true;
-        }else{
-            console.log(false); 
-            return false;
-        } 
-        
-    }
-    check('[]()', [['(', ')'], ['[', ']']]); // -> true
+
+
+if (arr.length == 0) {
+	console.log(true);
+	return true;
+}
+else {
+	console.log(false);
+	return false;
+}
+}
+check('((()))()', [['(', ')']]); // -> true
