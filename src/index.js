9ae2a65fa8284dc5372532f8b999e4b10f88621d
pzbel <katejidkova@gmail.com>
2018-02-28 14:23:07 +0300

    else loop

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = 
+    function check(str, bracketsConfig) {
     // your solution
     let arr = str.split("");
-    if (arr.length == 1) return false;
+    if (arr.length % 2 !== 0) return false;
     let lastOpen, lastIndexOpen, nextClosed;
-    let objOfBracket = {};
+    
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < bracketsConfig.length; j++) {
-            if (arr[i] == bracketsConfig[j[0]]) {
+            let configEl = bracketsConfig[j][0];
+            if (arr[i] == bracketsConfig[j][0]) {
                 lastOpen = arr[i];
                 nextClosed = arr[i + 1];
                 if (lastOpen !== nextClosed) {
                     lastIndexOpen = i;
-                //    console.log(lastOpen + " " + lastIndexOpen);
-                    if (nextClosed == bracketsConfig[j[1]]) {
-                        arr.splice(lastIndexOpen, 2);
-                //        console.log(arr);
+               //    console.log(lastOpen + " " + lastIndexOpen);
+                    if (nextClosed == bracketsConfig[j][1]) {
+                       arr.splice(lastIndexOpen, 2);
+              //         console.log(arr);
                     }
                 }
             }
