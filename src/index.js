f685eda46a8bed8a1227e290bda488ab30af3fe7
pzbel <katejidkova@gmail.com>
2018-02-28 15:18:07 +0300

    add mark

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
-module.exports = 
+//module.exports = 
     function check(str, bracketsConfig) {
-    // your solution
-    let arr = str.split("");
-    if (arr.length % 2 !== 0) return false;
-    let lastOpen, nextClosed;
-    
-    
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < bracketsConfig.length; j++) {
-            let configEl = bracketsConfig[j][0];
-            if (arr[i] == bracketsConfig[j][0]) {
-                lastOpen = arr[i];
-                nextClosed = arr[i + 1];
-                if (lastOpen !== nextClosed) {
-                    //console.log(lastOpen + " " + lastIndexOpen);
-                    if (nextClosed == bracketsConfig[j][1]) {
-                       arr.splice(i, 2);
-                       //console.log(arr);
-                    }
-                }
-            }
-        }
-    }
-    if (arr.length == 0) return true;
-    return false;
-}
+        // your solution
+        let arr = str.split("");
+        if (arr.length % 2 !== 0) return false;
+        let lastOpen, nextClosed;
 
-//check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
+        
+            
+             book:   for (let i = 0; i < arr.length; i++) {
+                    for (let j = 0; j < bracketsConfig.length; j++) {
+                        let configEl = bracketsConfig[j][0];
+                        if (arr[i] == bracketsConfig[j][0]) {
+                            lastOpen = arr[i];
+                            nextClosed = arr[i + 1];
+                            if (lastOpen !== nextClosed) {
+                                
+                                if (nextClosed == bracketsConfig[j][1]) {
+                                    arr.splice(i, 2);
+                                    continue book;
+                                }
+                            }
+                        }
+                    }
+                    if (arr.length !== 0) continue book;
+                }
+               
+            
+       
+        if (arr.length == 0){
+            console.log(true); 
+            return true;
+        }else{
+            console.log(false); 
+            return false;
+        } 
+        
+    }
+    check('[]()', [['(', ')'], ['[', ']']]); // -> true
