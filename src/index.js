99d5daa46af6aa22de26ea6433b2ee536df01a96
pzbel <katejidkova@gmail.com>
2018-02-28 10:15:18 +0300

    loop doesn't go to the end

--- src/index.js
+++ src/index.js
@@ -3,17 +3,31 @@
   // your solution
 	let arr = str.split("");
 	if(arr.length == 1) return false;
-	let arrLength = arr.length;
-	let lastOpen, lastIndexOpen;
+	let lastOpen, lastIndexOpen, nextClosed;
+    let objOfBracket = {};
 	
-	for( let i = 0; i < arrLength; i++){
+	for( let i = 0; i <  arr.length; i++){
 		if(arr[i] == "(" || arr[i] == "{" || arr[i] == "["){
 			lastOpen = arr[i];
+			nextClosed = arr[i + 1];
+            if(lastOpen !== nextClosed){
+                if(arr[i] == "(") objOfBracket["("] = ")";
+            if(arr[i] == "{") objOfBracket["{"] = "}";
+            if(arr[i] == "[") objOfBracket["["] = "]";
 			lastIndexOpen = i;
 			console.log(lastOpen + " " + lastIndexOpen);
+            if(nextClosed == objOfBracket["("]){
+                arr.splice(lastIndexOpen, 2);
+                console.log(arr);
+            } 
+            }
+            
 			
 		}
 	}
+        console.log(arr);
+        if(arr.length == 0) return true;
+        return false;
 }
 
 check('((()))()', [['(', ')']]) // -> true
