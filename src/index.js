c13ffc01c9b6aafcf140100535130207f92d7bbc
pzbel <katejidkova@gmail.com>
2018-02-28 13:58:07 +0300

    with second parametr

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-//module.exports = 
-	function check(str, bracketsConfig) {
-  // your solution
-	let arr = str.split("");
-	if(arr.length == 1) return false;
-	let lastOpen, lastIndexOpen, nextClosed;
+module.exports = function check(str, bracketsConfig) {
+    // your solution
+    let arr = str.split("");
+    if (arr.length == 1) return false;
+    let lastOpen, lastIndexOpen, nextClosed;
     let objOfBracket = {};
-	
-	for( let i = 0; i <  arr.length; i++){
-		if(arr[i] == "(" || arr[i] == "{" || arr[i] == "["){
-			lastOpen = arr[i];
-			nextClosed = arr[i + 1];
-            if(lastOpen !== nextClosed){
-                if(arr[i] == "(") objOfBracket["("] = ")";
-            if(arr[i] == "{") objOfBracket["{"] = "}";
-            if(arr[i] == "[") objOfBracket["["] = "]";
-			lastIndexOpen = i;
-			console.log(lastOpen + " " + lastIndexOpen);
-            if(nextClosed == objOfBracket["("]){
-                arr.splice(lastIndexOpen, 2);
-                console.log(arr);
-            } 
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (arr[i] == bracketsConfig[j[0]]) {
+                lastOpen = arr[i];
+                nextClosed = arr[i + 1];
+                if (lastOpen !== nextClosed) {
+                    lastIndexOpen = i;
+                //    console.log(lastOpen + " " + lastIndexOpen);
+                    if (nextClosed == bracketsConfig[j[1]]) {
+                        arr.splice(lastIndexOpen, 2);
+                //        console.log(arr);
+                    }
+                }
             }
-            
-			
-		}
-	}
-        console.log(arr);
-        if(arr.length == 0) return true;
-        return false;
+        }
+    }
+    if (arr.length == 0) return true;
+    return false;
 }
 
-check('((()))()', [['(', ')']]) // -> true
+//check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
